09fe3f49cc0b7b08363f9ddb41ad9486f54385f4
Henry Blandon <hblandon750@outlook.com>
2021-01-16 21:22:54 -0800

    changes from a clone in a diferent computer

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const findTheMisfit = (integers) => {
   // WRITE YOUR CODE HERE: ↓↓↓
   
   // The `integers` array contains all odd or all even integers
+  const odd = [1,3,4, 5, 7, 9, 11, 13,];
+  const even = [2, 4, 6 , 7, 10, 8, 12];
   // Except for one misfit that doesn't belong
   // Find the misfit in the array and return it
   // Type node app in the console below and hit enter to run the tests
